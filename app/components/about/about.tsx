8e28177dc7bf214a8baa9072a886272df3f9da2f
Azamat <azamat.kudziev@gmail.com>
2026-01-22 23:41:03 +0300

    rooms section

--- app/components/about/about.tsx
+++ app/components/about/about.tsx
@@ -1,23 +1,27 @@
-import Image from "next/image"
+import Image, { StaticImageData } from "next/image"
 import styles from "./about.module.css"
-import aboutPic from "../../../public/photos/about-1.jpg"
+import aboutPic from "/photos/about-1.jpg"
 
-export default function About() {
+export default function About({ children, description, bgPhoto }: {
+    children: React.ReactNode,
+    description: string,
+    bgPhoto: StaticImageData
+}) {
     return (
         <section className={styles['about-section']}>
             <div>
-                <h2 className={styles['title']}>Об отеле</h2>
+                <h2 className={styles['title']}>{children}</h2>
             </div>
             <div className={styles['section-photo']}>
                 <Image 
                     className={styles['photo']}
-                    src={aboutPic}
+                    src={bgPhoto}
                     alt="Фото об отеле"
                 />
                 <div className={styles['h5-and-p-box']}>
-                    <h5 className={styles['h5']}>Об отеле</h5>
+                    <h5 className={styles['h5']}>{children}</h5>
                     <p className={styles['paragraph']}>
-                        Парк-отель «Дзуарикау» расположен в живописном уголке Осетии, недалеко от Владикавказа и вблизи живописного поселка Фиагдон. Это идеальное место для отдыха на природе — в тишине, среди гор и чистого воздуха. Уютные номера, гостеприимная атмосфера и красота окружающих пейзажей сделают ваш отдых по-настоящему особенным.
+                        {description}
                     </p>
                 </div>
             </div>
--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,19 +1,27 @@
 import About from "../components/about/about";
 import ImageCarousel from "../components/carousel/carousel";
 import HeroSection from "../components/hero-section/hero-section";
+import restaurantPic from "../../public/photos/about-1.jpg"
+import roomsBg from "../../public/photos/roomsbg.png"
 
- const aboutImages = [
+ const restaurantImages = [
     { src: '/photos/about2.png', alt: 'Photo 1', title: 'Ресторан1' },
     { src: '/photos/about3.png', alt: 'Photo 2', title: 'Ресторан2' },
     { src: '/photos/about4.png', alt: 'Photo 3', title: 'Ресторан3' }
   ];
 
+  const restDescription = 'Парк-отель «Дзуарикау» расположен в живописном уголке Осетии, недалеко от Владикавказа и вблизи живописного поселка Фиагдон. Это идеальное место для отдыха на природе — в тишине, среди гор и чистого воздуха. Уютные номера, гостеприимная атмосфера и красота окружающих пейзажей сделают ваш отдых по-настоящему особенным.'
+  const roomsDescription = 'Уютные и стильные номера категории «люкс». Каждый номер оборудован всем необходимым для отдыха. Простор, тишина и вид на живописные пейзажи создают идеальные условия для полноценного расслабления и спокойного отдыха в гармонии с природой.'
+
+
+
 export default function MainPage() {
     return (
         <>
             <HeroSection />
-            <About />
-            <ImageCarousel images={aboutImages} />
+            <About description={restDescription} bgPhoto={restaurantPic} >{'Об отеле'}</About>
+            <ImageCarousel images={restaurantImages} />
+            <About description={roomsDescription} bgPhoto={roomsBg}>{'Номера'}</About>
         </>
     )
 }
