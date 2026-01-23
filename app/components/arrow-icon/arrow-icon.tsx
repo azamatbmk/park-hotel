25421b0e1176cf485330a0056cb0528a366b13e6
Azamat <azamat.kudziev@gmail.com>
2026-01-23 12:04:02 +0300

    arrows hover

---
+++ app/components/arrow-icon/arrow-icon.tsx
@@ -0,0 +1,19 @@
+export default function ArrowIcon({ color = 'currentColor' }) {
+  return (
+    <svg 
+      xmlns="http://www.w3.org/2000/svg" 
+      width="16" 
+      height="16" 
+      viewBox="0 0 16 16"
+      // Указываем fill здесь и в style
+      fill={color}
+      style={{ 
+        fill: color,
+        color: color,
+        flexShrink: 0 
+      }}
+    >
+      <path d="M16 16H14.4004V2.73145L1.13574 15.9961L0.00488281 14.8643L13.2686 1.60059H0V0.000976562H14.4004V0H16V16Z"/>
+    </svg>
+  );
+};
