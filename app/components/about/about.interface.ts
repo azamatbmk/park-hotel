5236ab987010e62f0004b8b32a3f3878229fb3c1
Azamat <azamat.kudziev@gmail.com>
2026-01-29 06:26:13 +0300

    Abot component

--- app/components/about/about.interface.ts
+++ app/components/about/about.interface.ts
@@ -2,7 +2,7 @@ import { StaticImageData } from "next/image";
 
 export interface IAboutProps {
     children: React.ReactNode;
-    description: string;
+    description?: string;
     descriptionPartOne?: string;
     descriptionPartTwo?: string;
     bgPhoto: StaticImageData;
