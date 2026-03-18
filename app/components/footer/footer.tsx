0a200fa634f58fb549b5d22f6d73c9be48a42546
Azamat <azamat.kudziev@gmail.com>
2026-03-18 21:27:31 +0300

    Fotter

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -26,71 +26,71 @@ export default function Footer() {
                     <p>© 2025. Парк-отель Дзуарикау</p>
                 </div>
             </div>
-
-            <div className={styles['footer-nav']}>
-                <h4 className={styles['h4']}>Навигация</h4>
-                <nav >
-                    <ul className={styles['nav-wrapper']} style={{ listStyle: 'none' }}>
-                        <li><Link href={'/'}>Об отеле</Link></li>
-                        <li><Link href={'/rooms'}>Номера</Link></li>
-                        <li><Link href={'/restaurant'}>Ресторан</Link></li>
-                        <li><Link href={'/pool'}>Бассейн & Баня</Link></li>
-                        <li><Link href={'/excursions'}>Экскурсии</Link></li>
-                        <li><Link href={'/contacts'}>Контакты</Link></li>
-                    </ul>
-                </nav>
-            </div>
-            <div className={styles['work-time']}>
-                <h4 className={styles['h4']}>График работы</h4>
-                <p>с 08:00 до 23:00</p>
-            </div>
-            <div className={styles['footer-adress']}>
-                <h4 className={styles['h4']}>Адрес</h4>
-                <div className={styles['adress-text']}>
-                    <Image
-                        src={pointIcon}
-                        alt={"Иконка геопозиции"}
-                        width={42}
-                        height={42}
-                    />
-                    <Link href={""}>
-                        Республика Северная Осетия — Алания,<br />село Дзуарикау, улица А. Кцоева, 25
-                    </Link>
+            <div className={styles['nav-work-adress-wrapper']}>
+                <div className={styles['footer-nav']}>
+                    <h4 className={styles['h4']}>Навигация</h4>
+                    <nav >
+                        <ul className={styles['nav-wrapper']} style={{ listStyle: 'none' }}>
+                            <li><Link href={'/'}>Об отеле</Link></li>
+                            <li><Link href={'/rooms'}>Номера</Link></li>
+                            <li><Link href={'/restaurant'}>Ресторан</Link></li>
+                            <li><Link href={'/pool'}>Бассейн & Баня</Link></li>
+                            <li><Link href={'/excursions'}>Экскурсии</Link></li>
+                            <li><Link href={'/contacts'}>Контакты</Link></li>
+                        </ul>
+                    </nav>
                 </div>
-                <PhoneButton />
-                <PhoneButtonMobile />
-                <div className={styles['social-logo-wrapper']}>
-                    <a href="" target="_blank">
-                        <Image
-                            src={"/whatsapp-logo.svg"}
-                            alt={"Whatsapp логотип"}
-                            width={40}
-                            height={40}
-                        />
-                    </a>
-                    <a href="" target="_blank">
-                        <Image
-                            src={"/instagram-logo.svg"}
-                            alt={"Instagram логотип"}
-                            width={40}
-                            height={40}
-                        />
-                    </a>
-                    <a href="" target="_blank">
-                        <Image
-                            src={"/telegram-logo.svg"}
-                            alt={"Telegram логотип"}
-                            width={40}
-                            height={40}
-                        />
-                    </a>
+                <div className={styles['work-time']}>
+                    <h4 className={styles['h4']}>График работы</h4>
+                    <p>с 08:00 до 23:00</p>
                 </div>
-                <div className={styles['policy-link-wrapper__mobile']}>
-                    <Link href={'/'} className={styles['policy-link']}>{'Правовая информация'}</Link>
-                    <p>© 2025. Парк-отель Дзуарикау</p>
+                <div className={styles['footer-adress']}>
+                    <h4 className={styles['h4']}>Адрес</h4>
+                    <div className={styles['adress-text']}>
+                        <Image
+                            src={pointIcon}
+                            alt={"Иконка геопозиции"}
+                            width={42}
+                            height={42}
+                        />
+                        <Link href={""}>
+                            Республика Северная Осетия — Алания,<br />село Дзуарикау, улица А. Кцоева, 25
+                        </Link>
+                    </div>
+                    <PhoneButton />
+                    <PhoneButtonMobile />
+                    <div className={styles['social-logo-wrapper']}>
+                        <a href="" target="_blank">
+                            <Image
+                                src={"/whatsapp-logo.svg"}
+                                alt={"Whatsapp логотип"}
+                                width={40}
+                                height={40}
+                            />
+                        </a>
+                        <a href="" target="_blank">
+                            <Image
+                                src={"/instagram-logo.svg"}
+                                alt={"Instagram логотип"}
+                                width={40}
+                                height={40}
+                            />
+                        </a>
+                        <a href="" target="_blank">
+                            <Image
+                                src={"/telegram-logo.svg"}
+                                alt={"Telegram логотип"}
+                                width={40}
+                                height={40}
+                            />
+                        </a>
+                    </div>
+                    <div className={styles['policy-link-wrapper__mobile']}>
+                        <Link href={'/'} className={styles['policy-link']}>{'Правовая информация'}</Link>
+                        <p>© 2025. Парк-отель Дзуарикау</p>
+                    </div>
                 </div>
             </div>
-
             <div className={`${path == '/luxroom' ? styles['bottom-div'] : ''}`}></div>
         </section>
     )
--- app/components/h2-header/h2-header.tsx
+++ app/components/h2-header/h2-header.tsx
@@ -2,7 +2,7 @@ import styles from "./h2-header.module.css"
 
 export default function H2HeaderComponent({ children }: { children: React.ReactNode }) {
     return(
-        <div>
+        <div >
             <h2 className={styles['title']}>{children}</h2>
         </div>
     )
